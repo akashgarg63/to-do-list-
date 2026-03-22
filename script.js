de29af5ef580bf8c86baf4fd7054c499e89bf260
akashgarg63 <akashgarg9193@gmail.com>
2026-03-23 03:05:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task.");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = taskText;
+
+  // Mark completed
+  li.onclick = function () {
+    li.classList.toggle("completed");
+  };
+
+  // Delete button
+  let deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "X";
+  deleteBtn.className = "delete-btn";
+
+  deleteBtn.onclick = function (e) {
+    e.stopPropagation();
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
